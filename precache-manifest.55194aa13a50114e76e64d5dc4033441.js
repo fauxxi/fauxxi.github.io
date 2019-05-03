ba3fd66c4544301ef1995319ca7287d1108a3880
fauxxi <fauxxi.as@gmail.com>
2019-05-03 14:04:26 +0200

    google translation api test

---
+++ precache-manifest.55194aa13a50114e76e64d5dc4033441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad85396fae196ad769971236b7bea32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b707c1c7cf288657171a",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "b707c1c7cf288657171a",
+    "url": "/static/js/main.24f7aa31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
